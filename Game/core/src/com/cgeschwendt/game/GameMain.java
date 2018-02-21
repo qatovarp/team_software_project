b9038f29d5a75a2b95ed988103cf69add6592d12
Cameron Geschwendt <cgeschwendt@rover-215-137.rovernet.mtu.edu>
2018-02-21 11:15:35 -0500

    test commit

--- Game/core/src/com/cgeschwendt/game/GameMain.java
+++ Game/core/src/com/cgeschwendt/game/GameMain.java
@@ -13,7 +13,7 @@ public class GameMain extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+	//	img = new Texture("badlogic.jpg");
 	}
 
 	@Override
@@ -21,7 +21,7 @@ public class GameMain extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 1, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+	//	batch.draw(img, 0, 0);
 		batch.end();
 	}
 	
--- README.md
+++
@@ -1,2 +0,0 @@
-# team_software_project
-this will be a test and deleted after the fact
