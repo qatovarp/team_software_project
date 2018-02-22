872f19fec561369983ce2c7b74bc8b7a0276d882
Cameron Geschwendt <cgeschwendt@cameron-gescwhwendt.local>
2018-02-21 21:50:27 -0500

    main menu started

---
+++ Game/core/src/HghScoreMenu/HighScoreMenu.java
@@ -0,0 +1,78 @@
+package HghScoreMenu;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.cgeschwendt.game.GameMain;
+import com.cgeschwendt.game.gameinfo.GameInfo;
+
+public class HighScoreMenu implements Screen {
+	private GameMain game;
+	private OrthographicCamera mainCamera;
+	private Viewport gameViewPort;
+	private Texture background;
+
+	public HighScoreMenu(GameMain game) {
+		this.game = game;
+
+		// sets up the main camera for the main menu.
+		mainCamera = new OrthographicCamera(GameInfo.WIDTH, GameInfo.HEIGHT);
+		mainCamera.position.set(GameInfo.WIDTH / 2f, GameInfo.HEIGHT / 2f, 0);
+		// sets the mainmenu viewport.
+		gameViewPort = new StretchViewport(GameInfo.WIDTH, GameInfo.HEIGHT, mainCamera);
+		
+		background = new Texture("");
+	}
+
+	@Override
+	public void show() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		game.getBatch().begin();
+		game.getBatch().draw(background,0,0);
+		game.getBatch().end();
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void pause() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void resume() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void hide() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void dispose() {
+		this.background.dispose();
+
+	}
+
+}
--- Game/core/src/com/cgeschwendt/game/GameMain.java
+++ Game/core/src/com/cgeschwendt/game/GameMain.java
@@ -1,33 +1,36 @@
 package com.cgeschwendt.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class GameMain extends ApplicationAdapter {
+import Mainmenu.MainMenu;
+
+
+public class GameMain extends Game {
+	// the one and only SpriteBatch for the game;
 	SpriteBatch batch;
-	Texture img;
+	
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-	//	img = new Texture("badlogic.jpg");
+		this.setScreen(new MainMenu(this));
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-	//	batch.draw(img, 0, 0);
-		batch.end();
+	super.render();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+	}
+	public SpriteBatch getBatch() {
+		return this.batch;
 	}
 }
