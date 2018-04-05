63b62f644eff068d809005d80e1248a78927489b
Quentin Tovar-Prince <qatovarp@users.noreply.github.com>
2018-04-05 16:39:35 -0400

    Made delete locks smoother. buffered the tilesheet so the rendering lines go away. Fixed a small player animation error

---
+++ Game/core/src/com/cgeschwendt/game/CustomQueryCallback.java
@@ -0,0 +1,39 @@
+package com.cgeschwendt.game;
+
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.QueryCallback;
+
+import objects.Item;
+
+public class CustomQueryCallback implements QueryCallback {
+	
+	private String objName;
+	private boolean foundOne;
+
+	public CustomQueryCallback(String objName) {
+		super();
+		this.objName = objName;
+		this.foundOne = false;
+	}
+	
+	@Override
+	public boolean reportFixture(Fixture fixture) {
+		if(fixture.getUserData() != null) {
+			System.out.println(fixture.getUserData());
+			if(fixture.getUserData().equals(this.objName)) {
+				((Item)fixture.getBody().getUserData()).destroy();
+				foundOne = true;
+			}
+			else {
+				//Remove this deletion doesn't work properly.
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public boolean foundOne() {
+		return foundOne;
+	}
+
+}
