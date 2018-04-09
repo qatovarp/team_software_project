7b1902b2fd8b6d356c72a2c01f85d40d80f6bf05
cgeschwendt <cameron@geschwendt.com>
2018-04-09 11:22:45 -0400

    sprite sheets

---
+++ Game/core/assets/TiledLevels/resources/more sprite.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="more sprite" tilewidth="70" tileheight="70" spacing="2" tilecount="81" columns="9">
+ <image source="stuff.png" width="646" height="646"/>
+</tileset>
